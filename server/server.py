0ee34bea12db025376fa8d6b6d3d4a9e13d274fe
Ignacio Pinto <jipinto99@gmail.com>
2024-09-27 18:51:33 -0300

    timeout try

--- server/server.py
+++ server/server.py
@@ -24,7 +24,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     while True:
         conn, addr = s.accept()
         #Add timeout to deal with packet loss
-        conn.settimeout(10.0)
+        conn.settimeout(3.0)
         with conn:
             print('Conectado por', addr)
             reception_over = True
@@ -42,74 +42,77 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     
                     elif(data.decode('utf-8') == "PACKAGE"):
                         print("Esperando paquete")
-                        data2 = conn.recv(1024)
-                        if data2:
-                            print("Recibido: ", data2)
-                            header = data2[:12]
-                            body = data2[12:]
+                        try:
+                            data2 = conn.recv(1024)
+                            if data2:
+                                print("Recibido: ", data2)
+                                header = data2[:12]
+                                body = data2[12:]
 
-                            # Separate header values and put in db
-                            mac = header[:6]
-                            mac_seq = ''.join([str(x) for x in mac])
+                                # Separate header values and put in db
+                                mac = header[:6]
+                                mac_seq = ''.join([str(x) for x in mac])
 
-                            msg = struct.unpack('H', header[6:8])[0] # msgID is 2 bytes unsigned
-                            protocol = header[8] # struct.unpack('B',header[8])
-                            transport = header[9] # struct.unpack('B',header[9])
-                            length = struct.unpack('H',header[10:12])[0]
+                                msg = struct.unpack('H', header[6:8])[0] # msgID is 2 bytes unsigned
+                                protocol = header[8] # struct.unpack('B',header[8])
+                                transport = header[9] # struct.unpack('B',header[9])
+                                length = struct.unpack('H',header[10:12])[0]
 
-                            print(f'mac: {mac_seq} | msg: {msg} | protocol: {protocol} | transport: {transport} | length: {length}')
+                                print(f'mac: {mac_seq} | msg: {msg} | protocol: {protocol} | transport: {transport} | length: {length}')
 
-                            # Save Mac Adress, only if it isn't saved already
-                            if not (Dev.select().where(Dev.mac_adress == mac_seq).exists()):
-                                Dev.insert(mac_adress=mac_seq).execute()
-                            dev_id = (Dev.select(Dev.device_id).where(Dev.mac_adress == mac_seq))
-                            print(dev_id)
-                            insert_id = Log.insert(
-                                msg_id = msg,
-                                device_id=dev_id,
-                                protocol_id=protocol,
-                                transport_layer=transport,
-                                length=length).execute()
+                                # Save Mac Adress, only if it isn't saved already
+                                if not (Dev.select().where(Dev.mac_adress == mac_seq).exists()):
+                                    Dev.insert(mac_adress=mac_seq).execute()
+                                dev_id = (Dev.select(Dev.device_id).where(Dev.mac_adress == mac_seq))
+                                print(dev_id)
+                                insert_id = Log.insert(
+                                    msg_id = msg,
+                                    device_id=dev_id,
+                                    protocol_id=protocol,
+                                    transport_layer=transport,
+                                    length=length).execute()
 
-                            # Acording to protocol, separate body values and put on db
-                            pack_id = insert_id #(Log.select(Log.packet_id).where(Log.msg_id == msg))
-                            print(f'PACKET_ID: {pack_id}')
-                            # All protocols send timestamp in the first 4 bytes
-                            timestamp = struct.unpack('I',body[0:4])[0]
-                            print(f'TIMESTAMP: {timestamp}')
+                                # Acording to protocol, separate body values and put on db
+                                pack_id = insert_id #(Log.select(Log.packet_id).where(Log.msg_id == msg))
+                                print(f'PACKET_ID: {pack_id}')
+                                # All protocols send timestamp in the first 4 bytes
+                                timestamp = struct.unpack('I',body[0:4])[0]
+                                print(f'TIMESTAMP: {timestamp}')
 
 
 
-                            if(protocol == 0):
-                                Data.insert(packet_id=pack_id,
-                                            timestamp=timestamp).execute()
-                            if(protocol == 1):
-                                batt = body[4]
-                                print(f'BATT: {batt}')
-                                Data.insert(
-                                    packet_id=pack_id,
-                                    timestamp=timestamp,
-                                    batt_level=batt).execute()
-                            if(protocol == 2):
-                                batt = body[4]
-                                temp = body[5]
-                                press = struct.unpack('I',body[6:10])[0]
-                                hum = body[10]
-                                print([x for x in body[11:]])
-                                print(f'BATT: {batt} TEMP:  {temp} PRESS:{press} HUM:{hum}')
-                                co = struct.unpack('f',body[11:])[0]
-                                print(f'BATT: {batt} TEMP:  {temp} PRESS:{press} HUM:{hum} CO:{co}')
-                                Data.insert(
-                                    packet_id=pack_id,
-                                    timestamp=timestamp,
-                                    batt_level=batt,
-                                    temp=temp,
-                                    press=press,
-                                    hum=hum,
-                                    co=co).execute()
-                            if(protocol == "3"):
-                                continue
-                            if(protocol == "4"):
-                                continue
+                                if(protocol == 0):
+                                    Data.insert(packet_id=pack_id,
+                                                timestamp=timestamp).execute()
+                                if(protocol == 1):
+                                    batt = body[4]
+                                    print(f'BATT: {batt}')
+                                    Data.insert(
+                                        packet_id=pack_id,
+                                        timestamp=timestamp,
+                                        batt_level=batt).execute()
+                                if(protocol == 2):
+                                    batt = body[4]
+                                    temp = body[5]
+                                    press = struct.unpack('I',body[6:10])[0]
+                                    hum = body[10]
+                                    print([x for x in body[11:]])
+                                    print(f'BATT: {batt} TEMP:  {temp} PRESS:{press} HUM:{hum}')
+                                    co = struct.unpack('f',body[11:])[0]
+                                    print(f'BATT: {batt} TEMP:  {temp} PRESS:{press} HUM:{hum} CO:{co}')
+                                    Data.insert(
+                                        packet_id=pack_id,
+                                        timestamp=timestamp,
+                                        batt_level=batt,
+                                        temp=temp,
+                                        press=press,
+                                        hum=hum,
+                                        co=co).execute()
+                                if(protocol == "3"):
+                                    continue
+                                if(protocol == "4"):
+                                    continue
+                                reception_over = False
+                        except:
                             reception_over = False
 
